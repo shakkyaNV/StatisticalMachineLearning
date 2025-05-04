262a846ef7b19d773190f60742f4dddaf8cc64a5
Shakkya Ranasinghe <shakkya.ranasingehe@gmail.com>
2025-05-04 03:17:54 -0500

    logging testing

---
+++ Code/test_run.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from datetime import datetime as dtime
+import logging
+
+
+## 1. Setup
+now = dtime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_path = f"../Logs/log_{now}.log"
+logging.basicConfig(
+    filename=log_path,
+    filemode="w",  # Overwrite each time; use "a" to append
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logging.info("Logging Started")
